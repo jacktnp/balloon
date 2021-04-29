e413e86e2f4ded72ecf04d470324771011f6053b
Narongded <32951598+Narongded@users.noreply.github.com>
2021-04-29 23:53:39 +0700

    profile

---
+++ balloon-frontend/src/pages/user/profile.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <navbar />
+        <div class="container">
+                 <h4 class="my-4 text-center">Profile</h4>
+                 <div class="row justify-content-center">
+                     <div class="col-4">
+                         <img src="" alt="">
+                     </div>
+                     <div class="col-4">112312</div>
+                 </div>
+        </div>
+       
+    </div>
+</template>
+
+<script>
+import navbar from '@/components/navbar';
+
+export default {
+    components: { navbar }
+}
+</script>
